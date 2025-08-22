524b1db02adc9ccb8198bc4bf3a2ac88f2a3ba27
Navya <chhokarnavya@gmail.com>
2025-08-22 18:34:45 +0000

    Complete 10_leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) return true;
+   return false;
 };
+// console.log(leapYears(1995));
 
 // Do not edit below this line
 module.exports = leapYears;
