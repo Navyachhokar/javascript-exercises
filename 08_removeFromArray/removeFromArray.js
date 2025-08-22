74ec52a045929ddd0b00ddbb3c4ad78c9dd15d01
Navya <chhokarnavya@gmail.com>
2025-08-22 13:42:12 +0000

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...itemToRemove) {
+    const flattened = itemToRemove.flat();
+    return array.filter(element => !flattened.includes(element));
 };
-
+// console.log(removeFromArray([1,2,4,5],[1,4]));
 // Do not edit below this line
 module.exports = removeFromArray;
