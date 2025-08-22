a4dc5fc5250abef4aebdd8f4044f8f0211f3008a
Navya <chhokarnavya@gmail.com>
2025-08-22 13:21:06 +0000

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.spilt().reverse().join();
+//  let str = "";
+//  for(let i = string.length - 1; i>= 0; i--){
+//     str += string[i];
+//  }
+//  return str;
 };
+// console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
