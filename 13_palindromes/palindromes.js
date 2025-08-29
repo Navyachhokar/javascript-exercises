fc8f3b909d1cc7ef950859c7607938eaaf78bca7
Navya <chhokarnavya@gmail.com>
2025-08-29 11:43:17 +0000

    Complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+   const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+   const cleanString = string.toLowerCase().split("").filter((char) => alphanumerical.includes(char)).join("");
+   const reverseString = cleanString.split("").reverse().join("");
+   return cleanString === reverseString;
 };
+// console.log(palindromes("nitin"));
 
 // Do not edit below this line
 module.exports = palindromes;
