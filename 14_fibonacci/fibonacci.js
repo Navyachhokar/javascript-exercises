537b66ad7bdcaeee5e538ffb79dad545f5b0c0ba
Navya <chhokarnavya@gmail.com>
2025-08-29 17:14:00 +0000

    Complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num === "0") return 0;
+   if(num<0) return "OOPS";
+   if(num === 0) return 0;
+   if(num === 1) return 1;
+   let current = 1;
+   let previous = 0;
+   for(let i = 2; i<=num; i++) {
+    let next = current + previous;
+    previous = current;
+    current = next;
+   }
+   return current;
 };
+// console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;
