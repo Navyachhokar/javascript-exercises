043ce3cc6e366de3128cf9321d353546ca7bf73b
Navya <chhokarnavya@gmail.com>
2025-08-29 11:24:13 +0000

    Complete 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
-
-const subtract = function() {
-	
+console.log(add(2,3));
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
-
-const sum = function() {
-	
+console.log(subtract(10,4));
+const sum = function(num1, num2) {
+	return num1 + num2;
 };
-
-const multiply = function() {
-
+console.log(sum(2,3));
+const multiply = function(num1, num2) {
+  return num1 * num2;
 };
-
-const power = function() {
-	
+console.log(multiply(2,4));
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
-
-const factorial = function() {
-	
+console.log(power(2,3));
+const factorial = function(num) {
+	let fact = 1;
+  if(num === 0 || num === 1) return 1;
+  for(let i = 1; i<=num; i++){
+    fact = fact * (i);
+  }
+  return fact;
 };
-
+console.log(factorial(5));
 // Do not edit below this line
 module.exports = {
   add,
